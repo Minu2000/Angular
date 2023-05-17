2976f534ef167ceb6bb1c49b4b8a3f15a57e7bcf
MINUMITHRA BALACHANDAR <minumithra.balachandar@gavait.com>
2023-05-17 12:44:58 +0530

    Created login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RegisterationComponent } from './registeration/registeration.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterationComponent
+    RegisterationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
