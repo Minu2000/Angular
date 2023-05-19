3c6f7ea0894635552be2f75bdb71cefe15b9d26a
MINUMITHRA BALACHANDAR <minumithra.balachandar@gavait.com>
2023-05-19 14:13:17 +0530

    login  form connected with backend register endpoint

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username:string ='';
+  email: string ='';
+  password: string='';
 
+
+
+constructor(private http: HttpClient, private router: Router) {}
+login(){
+  const url = 'http://localhost:5000/api/v1/login'; 
+    const loginData = {
+      username:this.username,
+      email: this.email,
+      password: this.password
+    };
+
+this.http.post(url, loginData)
+.subscribe(
+  {
+    next:  response => console.log(response),
+    error: error=>console.log(error),
+    //this.router.navigate(['/dashboard']);
+  },
+);
+}
 }
